cb16421ad8406eaceef3ac13f3991c4955ee2f87
xKenTangk57.xdx <86118516+xdx57@users.noreply.github.com>
2021-06-19 13:34:20 +0700

    Add files via upload

---
+++ p.php
@@ -0,0 +1,20 @@
+<?php
+function asu(){system('clear');}
+function ban($a){echo "\e[0m#subdomain scanner | coded by xKenTangk57.xdx\n#ip; \e[92m$a\e[0m\n\e[36m";}
+asu();
+echo "Masukan Url; ";
+$a = trim(fgets(STDIN));
+echo "Save to; ";
+$b = trim(fgets(STDIN));
+asu();
+ban($a);
+system("curl http://api.hackertarget.com/hostsearch/?q=".str_replace('http://','',str_replace('https://','',str_replace('www.','',strtolower($a))))." -o $b");
+asu();
+ban($a);
+$del = file_get_contents("$b");
+@unlink("$b");
+foreach(explode("\n",$del) as $lis) {
+$xdx = explode(',',$lis)[0];
+file_put_contents("$b","$xdx\n", FILE_APPEND);
+echo "~> $xdx\n";
+}
